529509aa9247eca2fe038a634a1b3573174b1ca6
Kristine Santana <kseraos@outlook.com>
2023-09-25 22:32:58 +0000

    desafio

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
     <>
       <div className='card'>
         <h2>Rick Sanchez</h2> 
+        <div className='card-descricao'>
+        <span>Status: Vivo</span>
+        <span>Espécie: Humana</span>
+        <span>Origem:Terra C-137</span>
+        </div>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+        
+      
       </div>
     </>
   )
